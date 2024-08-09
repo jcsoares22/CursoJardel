b5b930dd22c4fb260f89e1760bc93c63b619224b
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-08-08 21:29:04 -0300

    commit
    map

---
+++ dartPoo/funcao/map_recude_3.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Marina', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+
+  var notasFinais = alunos
+      .map((aluno) => aluno['nota'])
+      .map((nota) => (nota as double))
+      .where((nota) => nota >= 8);  
+
+  var total = notasFinais.reduce((t, a) => t + a);
+  print('A médias dos alunos e de ${total / notasFinais.length}');
+}
