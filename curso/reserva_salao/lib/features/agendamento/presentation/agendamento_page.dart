2fad055db69ada86c9a3e65db07d7df3f76333ca
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-11-14 19:27:42 -0300

    commit
    commitcommit

--- curso/reserva_salao/lib/features/agendamento/presentation/agendamento_page.dart
+++ curso/reserva_salao/lib/features/agendamento/presentation/agendamento_page.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-class AgendamentoPage extends StatefulWidget {
-  const AgendamentoPage({super.key});
-=======
 import 'package:go_router/go_router.dart';
-import 'package:reserva_salao/core/routes.dart';
 
 class AgendamentoPage extends StatefulWidget {
-  final String nome;
-  const AgendamentoPage({super.key, required this.nome});
->>>>>>> 44a29c4b38817a0a43d8096afe4da5dd4ff005f8
+  const AgendamentoPage(String s, {super.key});
 
   @override
   State<AgendamentoPage> createState() => _AgendamentoPageState();
@@ -20,12 +12,8 @@ class _AgendamentoPageState extends State<AgendamentoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-      body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text('data')),
-=======
       appBar: AppBar(
-        title: Text(widget.nome),
+        title: Text("nome"),
       ),
       body: Center(
         child: TextButton(
@@ -34,7 +22,6 @@ class _AgendamentoPageState extends State<AgendamentoPage> {
           },
           child: const Text("Voltar"),
         ),
->>>>>>> 44a29c4b38817a0a43d8096afe4da5dd4ff005f8
       ),
     );
   }
