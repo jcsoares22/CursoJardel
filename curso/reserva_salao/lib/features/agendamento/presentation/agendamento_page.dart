173ccbfc4f258c3e2af3c812bba3522ae276e6d9
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-11-14 19:10:56 -0300

    commit
    commit

---
+++ curso/reserva_salao/lib/features/agendamento/presentation/agendamento_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AgendamentoPage extends StatefulWidget {
+  const AgendamentoPage({super.key});
+
+  @override
+  State<AgendamentoPage> createState() => _AgendamentoPageState();
+}
+
+class _AgendamentoPageState extends State<AgendamentoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(onPressed: () {}, child: Text('data')),
+      ),
+    );
+  }
+}
